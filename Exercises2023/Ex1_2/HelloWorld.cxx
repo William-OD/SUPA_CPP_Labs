7dc51593d10181214b14d810c592af0123100cc7
William O'Donnell <86018743+William-OD@users.noreply.github.com>
2023-11-15 13:05:48 +0000

    added comments

--- Exercises2023/Ex1_2/HelloWorld.cxx
+++ Exercises2023/Ex1_2/HelloWorld.cxx
@@ -1,7 +1,16 @@
-#include <iostream>
+/* William O'Donnell 
+15/11/23
+C++ SUPA Course
+*/
 
-int main()
+#include <iostream> //Compiler directive to include the iostream standard file
+
+//'using namespace std;' is often used to avoid having to type std:: before every command. However it is considered bad practice as it can cause naming conflicts in complex projects using different namespaces.
+
+int main() //Main function
 {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+  std::cout << "Hello World!" << std::endl; 
+  //std is the standard namespace, from the iostream header file
+  //cout is the standard output stream, << is the insertion operator, endl is the end line manipulator These commands are pulled from the iostream header file
+  return 0; //Return 0 to the operating system (standard for main files)
 }
